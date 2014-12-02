a8e3bc1c3f623a8d3f4329254552402c3fc40ea5
IgorRyabtsev <igorryabtsev@inbox.ru>
2014-12-03 01:25:04 +0400

    Update prot.cpp

--- prot.cpp
+++ prot.cpp
@@ -22,11 +22,6 @@ void Convert(size_t size, char *a)
 	a[1]=mod;
 }
 
-size_t Getlen(char *a,size_t n)
-{
-	return 127*static_cast<size_t>(a[n])+1*static_cast<size_t>(a[n+1]);
-}
-
 void pars(string namefrom,string nameto, string message, char* arr)
 {
 	arr[0]=1;
@@ -77,49 +72,6 @@ void Convert(size_t size, boost::shared_array<char> a)
 	a[1]=mod;
 }
 
-size_t Getlen(boost::shared_array<char> a,size_t n)
-{
-	return 127*static_cast<size_t>(a[n])+1*static_cast<size_t>(a[n+1]);
-}
-void pars(string namefrom,string nameto, string message, boost::shared_array<char> arr)
-{
-	arr[0]=1;
-	arr[1]=0;
-	boost::shared_array<char> b(new char[2]);
-	int num=9+namefrom.size()+nameto.size()+message.size();
-	Convert(num,b);
-	arr[2]=b[0]; //celaya chast'
-	arr[3]=b[1]; //ostatok
-	arr[4]=1;
-	num=namefrom.size();
-	Convert(num,b);
-	arr[5]=b[0];
-	arr[6]=b[1];
-	for(size_t i=7, j=0; i<7+namefrom.size();i++, j++)
-	{
-		arr[i]=namefrom[j];
-	}
-	arr[7+namefrom.size()]=0;
-	num=nameto.size();
-	Convert(num,b);
-	arr[8+namefrom.size()]=b[0];
-	arr[9+namefrom.size()]=b[1];
-	for(size_t i=10+namefrom.size(), j=0; i<10+namefrom.size()+nameto.size();i++, j++)
-	{
-		arr[i]=nameto[j];
-	}
-	arr[10+namefrom.size()+nameto.size()]=2;
-	num=message.size();
-	Convert(num,b);
-	arr[11+namefrom.size()+nameto.size()]=b[0];
-	arr[12+namefrom.size()+nameto.size()]=b[1];
-	for(size_t i=13+namefrom.size()+nameto.size(), j=0; i<13+namefrom.size()+nameto.size()+message.size();i++, j++)
-	{
-		arr[i]=message[j];
-	}
-	arr[13+namefrom.size()+nameto.size()+message.size()]='\0';
-}
-
 class Error
 {};
 
@@ -138,57 +90,31 @@ public:
 			a[i]=v[i];
 	}
 
-	void vectempty(size_t ind)
-	{
-		size_t i=0;
-		v.erase(v.begin(),v.begin()+ind+1);
-		if(v.size() < 1)
-			return;
-		for(i=0; i<v.size()-1; i++){
-			if(v[i]==1 && v[i+1]==0){
-				break;
-			}
-		}
-		cout<< "Ishem i: " << i << endl;
-		if(i==v.size()-1){
-			if(v.back()==1){
-				v.erase(v.begin(),v.begin()+i);
-				return;
-			}
-			v.erase(v.begin(),v.begin()+i+1);
-			return;
-		}
-		v.erase(v.begin(),v.begin()+i);
-		cout << "vector [0]" << static_cast<int>(v[0]) << static_cast<int>(v[1]) << endl;
-	}
 	void parseData(char *data,size_t size)
 	{
+		Msg msg;
+		Error err;
+		if(size==0)
+			return;
+		for(size_t i = 0; i < size; i++)
+			v.push_back(data[i]);
 		while(true)
 		{
-			Msg msg;
-			Error err;
-			if(size==0)
-				return;
-			for(size_t i = 0; i < size; i++)
-				v.push_back(data[i]);
 			if(v.size()<4) // t.k. ne doshli daje do razmera
 				return;
 			boost::shared_array<char> a(new char[v.size()]);
 			vecttoarr(a);//pishem vse chto v vectore v massiv, chtobi obrabativat'
-			size_t len=Getlen(a,2); //dlinu ostavsheisya chasti poluchili, posze obshego size
-			if(v.size()<len+4)//ne prishlo polnoe soobshenie
-				return;
-			if(len<9) // nevozmojnii variant, t.k. minimum 9 yacheek
-				return;			
 			if(a[0]!=1){ 			//ne nasha versiya protokola
-				vectempty(0);
 				throw err;
 			}
-
 			if(a[1]!=0){			//ne nasha versiya protokola
-				vectempty(1);
 				throw err;
 			}
+			size_t len=Getlen(a,2); //dlinu ostavsheisya chasti poluchili, posze obshego size
+			if(len<9) // nevozmojnii variant, t.k. minimum 9 yacheek
+				throw err;
+			if(v.size()<len+4)//ne prishlo polnoe soobshenie
+				return;	
 			if(a[4]==1)
 			{
 				size_t size=Getlen(a,5);//static_cast<size_t>(a[4]);
@@ -234,7 +160,6 @@ public:
 						}
 					}
 					else{
-						vectempty(Getlen(a,5));
 						throw err; //
 					}
 			if(a[7+Getlen(a,5)]==0)
@@ -279,7 +204,6 @@ public:
 						}
 					}
 					else{
-						vectempty(7+Getlen(a,5));
 						throw err;
 					}//
 			if(a[10+Getlen(a,5)+Getlen(a,8+Getlen(a,5))]==2)
@@ -327,7 +251,6 @@ public:
 						}
 					}
 					else{
-						vectempty(10+Getlen(a,5)+Getlen(a,8+Getlen(a,5)));
 						throw err; //
 					}
 			//esli doshli do etogo momenta, to est' minimum 1 soobshenie i ego kidaem v vector, udalyaem iz vectora v elementi 1 soobsheniya
@@ -343,153 +266,6 @@ private:
 	vector<char> v;
 };
 
-void findresult(boost::shared_array<char> a,Msg &msg)
-{
-	if(a[0]==1 && a[1]==0)
-	{
-		size_t len=Getlen(a,2);//static_cast<size_t>(a[2]);
-		if(len>9)
-		{
-			if(a[4]==1)
-			{
-				size_t size=Getlen(a,5);//static_cast<size_t>(a[4]);
-				if(size>0)
-				{
-					boost::shared_array<char> arr(new char [size+1]);
-					for(size_t i=7, j=0;i<7+size && j<size ;i++,j++)
-					{
-						arr[j]=a[i];
-					}
-					arr[size]='\0';
-
-					msg.otkogo=static_cast<string>(arr.get());
-				}
-			}
-			else 
-				if(a[4]==0)
-				{
-					size_t size=Getlen(a,5);//static_cast<size_t>(a[4]);
-					if(size>0)
-					{
-						boost::shared_array<char> arr(new char [size+1]);
-						for(size_t i=7, j=0;i<7+size && j<size ;i++,j++)
-						{
-							arr[j]=a[i];
-						}
-						arr[size]='\0';
-						msg.komu=static_cast<string>(arr.get());
-					}
-				}
-				else
-					if(a[4]==2)
-					{
-						size_t size=Getlen(a,5);//static_cast<size_t>(a[4]);
-						if(size>0)
-						{
-							boost::shared_array<char> arr(new char [size+1]);
-							for(size_t i=7, j=0;i<7+size && j<size ;i++,j++)
-							{
-							arr[j]=a[i];
-							}
-							arr[size]='\0';
-							msg.message=static_cast<string>(arr.get());
-						}
-					}
-					else throw "Ne podderjivaetsya etot protokol!";
-			if(a[7+Getlen(a,5)]==0)
-			{
-				size_t size=Getlen(a,8+Getlen(a,5));
-				{
-					boost::shared_array<char> arr(new char [size+1]);
-					for(size_t i=10+Getlen(a,5), j=0;i<10+Getlen(a,5)+size;i++,j++)
-					{
-						arr[j]=a[i];
-					}
-					arr[size]='\0';
-					msg.komu=static_cast<string>(arr.get());
-				}
-			}
-			else
-				if(a[7+Getlen(a,5)]==1)
-				{
-					size_t size=Getlen(a,8+Getlen(a,5));
-					{
-						boost::shared_array<char> arr(new char[size]);
-						for(size_t i=10+Getlen(a,5), j=0;i<10+Getlen(a,5)+size;i++,j++)
-						{
-							arr[j]=a[i];
-						}
-						arr[size]='\0';
-						msg.otkogo=static_cast<string>(arr.get());
-					}
-				}
-				else
-					if(a[7+Getlen(a,5)]==2)
-					{
-						size_t size=Getlen(a,8+Getlen(a,5));
-						{
-							boost::shared_array<char> arr(new char [size]);
-							for(size_t i=10+Getlen(a,5), j=0;i<10+Getlen(a,5)+size;i++,j++)
-							{
-								arr[j]=a[i];
-							}
-							arr[size]='\0';
-							msg.message=static_cast<string>(arr.get());
-						}
-					}
-					else throw "Ne podderjivaetsya etot protocol";
-
-			if(a[10+Getlen(a,5)+Getlen(a,8+Getlen(a,5))]==2)
-			{
-				size_t size=Getlen(a,11+Getlen(a,5)+Getlen(a,8+Getlen(a,5)));//static_cast<size_t>(a[8+static_cast<size_t>(a[4])+static_cast<size_t>(a[6+static_cast<size_t>(a[4])])]);
-				if(size>0)
-				{
-					boost::shared_array<char> arr(new char [size+1]);
-					for(size_t i=13+Getlen(a,5)+Getlen(a,8+Getlen(a,5)), j=0;i<13+Getlen(a,5)+Getlen(a,8+Getlen(a,5))+size;i++,j++)
-					{
-						arr[j]=a[i];
-					}
-					arr[size]='\0';
-					msg.message=static_cast<string>(arr.get());
-				}
-			}
-			else
-				if(a[10+Getlen(a,5)+Getlen(a,8+Getlen(a,5))]==1)
-				{
-					size_t size=Getlen(a,11+Getlen(a,5)+Getlen(a,8+Getlen(a,5)));//static_cast<size_t>(a[8+static_cast<size_t>(a[4])+static_cast<size_t>(a[6+static_cast<size_t>(a[4])])]);
-					if(size>0)
-					{
-						boost::shared_array<char> arr(new char [size]);
-						for(size_t i=13+Getlen(a,5)+Getlen(a,8+Getlen(a,5)), j=0;i<13+Getlen(a,5)+Getlen(a,8+Getlen(a,5))+size;i++,j++)
-						{
-							arr[j]=a[i];
-						}
-						arr[size]='\0';
-						msg.otkogo=static_cast<string>(arr.get());
-					}
-				}
-				else 
-					if(a[10+Getlen(a,5)+Getlen(a,8+Getlen(a,5))]==0)
-					{
-						size_t size=Getlen(a,11+Getlen(a,5)+Getlen(a,8+Getlen(a,5)));//static_cast<size_t>(a[8+static_cast<size_t>(a[4])+static_cast<size_t>(a[6+static_cast<size_t>(a[4])])]);
-						if(size>0)
-						{
-							boost::shared_array<char> arr(new char [size]);
-							for(size_t i=13+Getlen(a,5)+Getlen(a,8+Getlen(a,5)), j=0;i<13+Getlen(a,5)+Getlen(a,8+Getlen(a,5))+size;i++,j++)
-							{
-								arr[j]=a[i];
-							}
-							arr[size]='\0';
-							msg.komu=static_cast<string>(arr.get());
-						}
-					}
-					else throw "Ne podderjivaetsya etot protocol"; //
-		}
-		else throw "Ne podderjivaetsya etot protocol";
-	}
-	else throw "ne podderjivaetsya etot protocol";
-}
-
 int main()
 {
 	string message="message";
@@ -500,27 +276,28 @@ int main()
 	char *a=new char[len];
 	pars(namefrom,nameto,message,a);
 	Parser parser;
-	for(size_t i=0; i<30; i++)
-	{
-		try{
+	a[0]=0;
+	
+	try{
+		for(size_t i=0; i<30; ++i)
+		{
 			parser.parseData(a+i,1);
 			if(parser.vmes.size()!=0)
 			{
-				cout << i << endl;
 				m=parser.vmes[0];
 			}
 		}
-		catch(Error )
-		{
-			cerr << "ne podderjivaetsya" << endl;
-		}
 	}
-	cout << parser.vmes.size() << endl;
+	catch(Error )
+	{
+		cerr << "ne podderjivaetsya" << endl;
+	}
+	
 
+	cout << parser.vectlen() << endl;
 	cout<<m.otkogo<<endl;
 	cout<<m.komu<<endl;
 	cout<<m.message<<endl;
 
-	cout << parser.vectlen() << endl;
 return 0;
 }
